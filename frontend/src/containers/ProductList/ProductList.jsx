61c2bfe8522ff342bcd8ad7be94009a6c1093217
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-03-16 04:35:23 +0100

    starting

---
+++ frontend/src/containers/ProductList/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar';
+const ProductList = () => {
+  return (
+    <>
+    <div className="container">
+        <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default ProductList
