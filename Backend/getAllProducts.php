9635ea2d56998c838e23ef93320da52b4bae96d6
Roqeebah Abdulganiy <93500396+Chandrasura25@users.noreply.github.com>
2023-04-13 04:05:41 -0700

    Update and rename getAllProducts.php to index.php

--- Backend/getAllProducts.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "classes/Product.php";
-$product = new Product("","","","");
-$data = $product->getProductData();
-// print_r($data['result']);
-
-// print_r($getAllProducts['result']);
- echo json_encode($data);
-?>
