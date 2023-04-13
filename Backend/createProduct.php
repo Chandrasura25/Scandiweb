44270cfb07a97e7e1912c0eecc1a808f140cbebf
Roqeebah Abdulganiy <93500396+Chandrasura25@users.noreply.github.com>
2023-04-13 04:06:34 -0700

    Update createProduct.php

--- Backend/createProduct.php
+++ Backend/createProduct.php
@@ -1,7 +1,11 @@
 <?php
 
 require "classes/Product.php";
-$_POST = json_decode(file_get_contents("php://input"),true);
+
+$_POST = json_decode(file_get_contents("php://input"), true);
+if(isset($_POST['sku'])){
+
+// Sanitize the input data
 $sku = $_POST['sku'];
 $name = $_POST['name'];
 $price = $_POST['price'];
@@ -11,7 +15,18 @@ $weight = $_POST['weight'];
 $height = $_POST['height'];
 $width = $_POST['width'];
 $length = $_POST['length'];
-
+// Create a new product object
 $product = new Product($sku, $name, $price, $productType, $size, $weight, $height, $width, $length);
-$saved = $product->save();
+
+// Save the product and get the result
+$saved = $product->save($sku, $name, $price, $productType, $size, $weight, $height, $width, $length);
+
+// Send the response as JSON
+header("Content-Type: application/json");
 echo json_encode($saved);
+
+}
+else {
+    echo json_encode(['success' => false, 'message' => 'Invalid Request']);
+}
+?>
